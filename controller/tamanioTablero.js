0d86f939f934100d4c9acec053f45708db21211e
Solorza <david.josue.solorza@gmail.com>
2025-03-28 04:11:24 -0400

    logrado
    se logro combinar los srcript slo falta acomodar los container para que despues de los 12 cuadros no se sobre pongan

--- controller/tamanioTablero.js
+++ controller/tamanioTablero.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     boton.addEventListener("click", function () {
         let num = input.value.trim();
 
-        // Validar que el número sea 3, 5 o 7
+        
         if (num < 10 || num > 20) {
             alert("Solo se permiten los números entre 10 y 20 (Incluidos estos)");
             input.value = ""; // Borra el campo si el número es incorrecto
