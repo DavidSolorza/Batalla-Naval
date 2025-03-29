7b04b5f8b61d04d7278fd1a737ad0a4d27699235
Luisa Ibarra <ibarratucano@gmail.com>
2025-03-29 17:31:22 -0500

    cuadroPregunta

--- controller/tamanioTablero.js
+++ controller/tamanioTablero.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     let boton = document.getElementById("jugar");
     let input = document.getElementById("tamanio");
+    let selectionModal = document.getElementById("selectionModal");
 
+    selectionModal.style.display = "none"; // Ocultar modal
+    
     boton.addEventListener("click", function () {
         let num = input.value.trim();
 
@@ -10,9 +13,17 @@ document.addEventListener("DOMContentLoaded", function () {
             alert("Solo se permiten los números entre 10 y 20 (Incluidos estos)");
             input.value = ""; // Borra el campo si el número es incorrecto
             return;
-        }
+        } 
 
-        // Redireccionar a la página del tablero correspondiente
-        window.location.href = `tablero.html?size=${num}`;
+        // Mostrar el modal
+        selectionModal.style.display = "flex";
+
+        // Asignar eventos a los botones del modal
+        document.getElementById("manualBtn").addEventListener("click", function () {
+            window.location.href = `tablero.html?size=${num}`;
+        });
+
+        document.getElementById("autoBtn").addEventListener("click", function () {
+            window.location.href = `tableroAuto.html?size=${num}`;        });
     });
 })
