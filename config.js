26f56a9c0d09b44b7cfd4879be3d78a3db26c5d1
Solorza <david.josue.solorza@gmail.com>
2025-04-04 21:28:07 -0400

    exportacion de los mapas

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const CONFIG = {
     API_RANKING: "http://127.0.0.1:5000/ranking",
     API_COUNTRIES: "http://127.0.0.1:5000/countries",
-    API_BANDERAS: "https://flagsapi.com/#sizes"
+    API_BANDERAS: "https://flagsapi.com/#sizes",
+    API_CLIMA: "https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m",
+    API_KEY: "37ff0bd2cad509cdf73fa73ff1e61e94" // Reemplaza esto con tu clave válida
 };
 
 export default CONFIG;
